880798d834f4ee5ab2d385e226fdf57aaaec6a4d
suchartbu@gmail.com <suchartbu@gmail.com@2a67e68e-c06c-11de-a0e9-83d03c287164>
2010-07-30 09:33:13 +0000

    เพิม orr_lib ตัวที่ถูก

---
+++ trunk/orr-projects/orr_lib/gui/OrList.php
@@ -0,0 +1,27 @@
+<?php
+ /******************************************************************
+ * @version php5
+ * @author Suchart Bunhachirat
+ * @copyright Copyright &copy; 2007, orr
+ * Class สร้างไฟล์ Table List
+ *******************************************************************/
+classOrList extends OrGui{
+	/**
+	 *  skin for create HTML List
+	 *  @access public
+	  **/
+	public $skin = nulll;
+	/**
+	 * __construct : วิธีการทำงานเริ่มต้นของคลาส
+	 * @param string title Page Title
+     * @param string
+	 * @return null
+	 */
+	 function __construct($title = '' ){
+	 	/**
+	 	 * กำหนดค่าเริ่มต้นสำหรับสร้าง HTML LIST
+	 	 * */
+			
+		}
+}
+?>
