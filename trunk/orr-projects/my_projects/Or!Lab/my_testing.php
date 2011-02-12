5a942141d1ae2064b4b9b3ad73591eb992970a8b
suchartbu@gmail.com <suchartbu@gmail.com@2a67e68e-c06c-11de-a0e9-83d03c287164>
2011-02-12 09:30:10 +0000

    ปรับส่วนติดต่อกับผู้ใช้งานให้เป็นระบบ

---
+++ trunk/orr-projects/my_projects/Or!Lab/my_testing.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * หน้าจอสำหรับทดสอบการเขียนโปรแกรม
+ * 1. ทดสอบการสร้างหน้าจอ
+ * 2. ทดสอบการรับและส่งค่า ระหว่างโปรแกรม
+ */
+require_once('../../orr_lib/Or.php');
+require_once('config.inc.php');
+/**
+ * Class สร้างหน้าจอ HTML
+ * @package    Or!Lib
+ * @author     Suchart Bunhachirat <suchart.orr@gmail.com>
+ * @copyright  1997-2005 The PHP Group
+ * @license
+ * @version    2554
+ */
+class my_page extends OrHtml {
+
+    function __construct($title = '') {
+        global $my_cfg;
+        parent::__construct($title);
+        $this->set_skin($my_cfg[skins_path] . 'default_testing.html');
+    }
+
+}
+
+$my = new my_page('Orr-projects ทดสอบระบบ');
+$my->show();
+?>
