c5eae4d08ffe6fe368d61757824b6f45ffa6088a
suchart.orr <suchart.orr@2a67e68e-c06c-11de-a0e9-83d03c287164>
2011-08-30 12:45:17 +0000

    แก้ไขหน้าจอการบันทึกข้อมูล (my_form) ให้รองรับการทำงาน เป็นระบบมากขึ้น (ยังไม่เสร็จ)

---
+++ trunk/orr-projects/my_projects/_config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * ค่าที่กำหนดใช้งานร่วมกันใน _page.cls.php
+ * @package   my_projects
+ * @author     Suchart Bunhachirat <suchart.orr@gmail.com>
+ * @copyright  
+ * @license    
+ * @version    2554
+ */
+
+/*
+ * กำหนดโฟลเดอร์ที่เรียกใช้งานไฟล์ สำหรับตกแต่งหน้าจอระบบร่วมกัน
+ */
+$my_cfg[skins_path] = "../../skins/default_new/";
+?>
