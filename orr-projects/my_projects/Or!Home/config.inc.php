9c49e66a2469f7d53f5f22a196b8888991304859
suchart.orr <suchart.orr@2a67e68e-c06c-11de-a0e9-83d03c287164>
2011-08-30 12:45:17 +0000

    แก้ไขหน้าจอการบันทึกข้อมูล (my_form) ให้รองรับการทำงาน เป็นระบบมากขึ้น (ยังไม่เสร็จ)

--- orr-projects/my_projects/Or!Home/config.inc.php
+++ orr-projects/my_projects/Or!Home/config.inc.php
@@ -1,5 +1,21 @@
 <?php
-$my_cfg[db]="or!config";
-$my_cfg[skins_path]="../../skins/default_new/";
-$my_cfg[title]="ระบบงานสารสนเทศ และสมาชิก";
+
+/*
+ * ค่าที่กำหนดใช้งานร่วมกันใน หน้าจอที่ใช้งานร่วมกัน
+ * @package   my_projects
+ * @author     Suchart Bunhachirat <suchart.orr@gmail.com>
+ * @copyright  
+ * @license    
+ * @version    2554
+ */
+
+/*
+ * $my_cfg[db]="or!config"; กำหนดชื่อฐานข้อมูลที่ใช้งานร่วมกัน
+ */
+$my_cfg[db] = "or!config";
+
+/*
+ * $my_cfg[title] = "ระบบงานสารสนเทศ และสมาชิก"; กำหนดชื่อระบบงาน TODO : ทำให้สามารถกำหนดได้จากระบบ
+ */
+$my_cfg[title] = "ระบบงานสารสนเทศ และสมาชิก";
 ?>
